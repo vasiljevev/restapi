c7d05929c28fe32d433700e5272b04a75a1f3a43
e-vasilyev <e-vasilyev@it-serv.ru>
2023-11-03 11:35:44 +0300

    year

--- examples/python/helloworld/README.md
+++ examples/python/helloworld/README.md
@@ -2,3 +2,9 @@
 
 
 //https://grpc.io/docs/languages/python/quickstart/
+
+https://syntaxbug.com/a72bddde1b/
+
+https://habr.com/ru/articles/590679/
+
+https://habr.com/ru/articles/770564/
--- examples/python/helloworld/db_conect.py
+++ examples/python/helloworld/db_conect.py
@@ -8,16 +8,18 @@ DRIVER = 'SQL Server'
 connectionString = f'DRIVER={DRIVER};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes'
 conn = pyodbc.connect(connectionString)
 
-def select(self, request):
+#request="SELECT top 10 link FROM dbo.ED_Meter_Readings order by link asc"
+
+def select(request):
     conn = pyodbc.connect(connectionString)
     cursor = conn.cursor() # создаем курсор
     cursor.fast_executemany = True  # активируем быстрое выполнение
-
-    # создаём заготовку для создания таблицы (начало)
-    #query = "SELECT top 1 C_Name  FROM dbo.SD_divisions"
     cursor.execute(request)  # execute
-    #cursor.execute("SELECT top 1 C_Name  FROM dbo.SD_divisions")
     row = cursor.fetchone()
+    for r in row:
+        print(r)
     #print(row)
-    return row
     conn.close()
+    return row
+
+#select(request)
